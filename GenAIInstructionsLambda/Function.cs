f95e387fc2cc38c91f7c740cad9d31d109339b8f
Jose Morales <jmcmilian686@gmail.com>
2025-03-13 20:19:00 -0400

    Updating input message

--- GenAIInstructionsLambda/Function.cs
+++ GenAIInstructionsLambda/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,15 +8,22 @@ namespace GenAIInstructionsLambda;
 
 public class Function
 {
-    
+    public class Input
+    {
+        public string Property1 { get; set; }
+        public int Property2 { get; set; }
+        // Add other properties as needed
+    }
+
     /// <summary>
-    /// A simple function that takes a string and does a ToUpper
+    /// A simple function that takes an input object and returns a greeting message.
     /// </summary>
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public string FunctionHandler(Input input, ILambdaContext context)
     {
-        return("Hello From Lambda: "+input);
+        string serialized = JsonSerializer.Serialize(input);
+        return ("Hello From Lambda: " + serialized);
     }
 }
